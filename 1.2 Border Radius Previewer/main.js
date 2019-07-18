74ae95ab0f6680b9a0175b8c24bc8379ef7df3d0
Matt <eclecticappdevelopment@gmail.com>
2019-07-18 18:42:24 +0100

    1.2 roughly complete
    Rough code but works fine - adjust the 4 values and see the preview. Might adjust to fire "onchange" rather than "onsubmit" but functionally complete.

---
+++ 1.2 Border Radius Previewer/main.js
@@ -0,0 +1,41 @@
+/*
+
+  The main function to convert,
+  which calls the helper functions
+  to convert (manually or cheatingly)
+
+*/
+
+var cssDefault = `
+  height: 150px;
+  width: 150px;
+  color: white;
+  text-align: center;
+  vertical-align: middle;
+  background-color: #a0b;
+  border: 1px solid black;
+`;
+
+function updateBorderRadius(event){
+
+  event.preventDefault();
+
+  let topLeftValue = document.getElementById('top-left-radius-input').value;
+  let topRightValue = document.getElementById('top-right-radius-input').value;
+  let btmLeftValue = document.getElementById('bottom-left-radius-input').value;
+  let btmRightValue = document.getElementById('bottom-right-radius-input').value;
+
+  let cssString = 'border-radius: ' + topLeftValue + 'px ' + topRightValue + 'px ' + btmRightValue + 'px ' + btmLeftValue + 'px; ';
+
+  console.log(cssString);
+  cssString = cssString + cssDefault;
+
+  let resultsDiv = document.getElementById('border-preview-div');
+  resultsDiv.setAttribute('style', cssString);
+
+  let resultsSpan = document.getElementById('result-span');
+  resultsSpan.innerHTML = cssString;
+
+  console.log('Ended function');
+  return false;
+}
